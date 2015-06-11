c88611b7aa4a15ac40673260048c486e7fdde6a4
Nicolas Dubois <nicolas.dubois1@viacesi.fr>
2015-06-11 09:08:06 +0200

    procedure correction

--- src/com/towerdefense/towerdefense/database/DBProcedure.java
+++ src/com/towerdefense/towerdefense/database/DBProcedure.java
@@ -1,87 +1,80 @@
 package com.towerdefense.towerdefense.database;
 
 public abstract class DBProcedure {
-    public static String loadTerrain(){
-        return "{call loadTerrain(?)}";
-    }
-    
-    public static String getIDPlayer(){
-        return "{call getIDPlayer(?)}";
-    }
-
-    public static String selectAllMaps(){
-        return "{call selectAllMaps()}";
-    }
-    
-    public static String selectAllSaves(){
-        return "{call selectAllSaves()}";
-    }
-
-    
-    public static String getTerrain() {
-        return "{? = call getterrain[(?, ?)]}";
-    }
-
-    public static String setSave() {
-        return "{call setSave[(?, ?, ?, ?, ?, ?)]}";
-    }
-
-    public static String setScore() {
-        return "{call setScore[(?, ?)]}";
-    }
-
-        
-    public static String getScore() {
-	    return "{? = call getScore[(?)]}";
+	public static String getIDPlayer() {
+		return "{call getIDPlayer(?)}";
 	}
-	
-	public static String loadPlayerSaveWithIDSave() {
-	    return "{? = call loadPlayerSaveWithIDSave[(?)]}";
-	}
-	
-	public static String loadPlayerWithID() {
-	    return "{? = call loadPlayerWithID[(?)]}";
-	}
-	
+
 	public static String getSave() {
-        return "{? = call loadSaveWithID[(?)]}";
-    }
-	
-	public static String loadTerrainWithID() {
-	    return "{? = call loadTerrainWithID[(?)]}";
+		return "{? = call loadSaveWithID(?)}";
 	}
-	
-	    
-	public static String loadTowerSaveWithIDSave() {
-        return "{? = call loadTowerSaveWithIDSave[(?)]}";
-    }
-	
-	public static String loadTowerWithID() {
-        return "{? = call loadTowerWithID[(?)]}";
-    }
-    
-	public static String mapSelection() {
-        return "{? = call mapSelection[(?)]}";
-    }
-    
-	public static String saveMap() {
-	    return "{call saveMap[(?, ?, ?)]}";
-	}
-	
-	public static String savePlayer() {
-	    return "{call savePlayer[(?)]}";
-	}
-	
-	public static String setTerrain() {
-        return "{call saveTerrain[(?, ?, ?, ?)]}";
-    }
-	
-	public static String saveTower() {
-	    return "{call saveMap[(?, ?, ?, ?, ?, ?)]}";
-	}
-		
-   
-    
-    
-}
 
+	public static String getScore() {
+		return "{? = call getScore(?)}";
+	}
+
+	public static String getTerrain() {
+		return "{? = call getterrain(?, ?)}";
+	}
+
+	public static String loadPlayerSaveWithIDSave() {
+		return "{? = call loadPlayerSaveWithIDSave(?)}";
+	}
+
+	public static String loadPlayerWithID() {
+		return "{? = call loadPlayerWithID(?)}";
+	}
+
+	public static String loadTerrain() {
+		return "{call loadTerrain(?)}";
+	}
+
+	public static String loadTerrainWithID() {
+		return "{? = call loadTerrainWithID(?)}";
+	}
+
+	public static String loadTowerSaveWithIDSave() {
+		return "{? = call loadTowerSaveWithIDSave(?)}";
+	}
+
+	public static String loadTowerWithID() {
+		return "{? = call loadTowerWithID(?)}";
+	}
+
+	public static String mapSelection() {
+		return "{? = call mapSelection(?)}";
+	}
+
+	public static String saveMap() {
+		return "{call saveMap(?, ?, ?)}";
+	}
+
+	public static String savePlayer() {
+		return "{call savePlayer(?)}";
+	}
+
+	public static String saveTower() {
+		return "{call saveMap(?, ?, ?, ?, ?, ?)}";
+	}
+
+	public static String selectAllMaps() {
+		return "{call selectAllMaps()}";
+	}
+
+	public static String selectAllSaves() {
+		return "{call selectAllSaves()}";
+	}
+
+	public static String setSave() {
+		return "{call setSave(?, ?, ?, ?, ?, ?)}";
+	}
+
+	public static String setScore() {
+		return "{call setScore(?, ?)}";
+	}
+
+	public static String setTerrain() {
+		return "{call saveTerrain(?, ?, ?, ?)}";
+	}
+
+}
