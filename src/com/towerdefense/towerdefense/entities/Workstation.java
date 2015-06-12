9eb720b578283446d2bce8a149f859f9dff3e1db
balzrog <adrien.laffargue@viacesi.fr>
2015-06-12 08:43:27 +0200

    repair

--- src/com/towerdefense/towerdefense/entities/Workstation.java
+++ src/com/towerdefense/towerdefense/entities/Workstation.java
@@ -11,8 +11,7 @@ import com.towerdefense.towerdefense.entities.towers.Tower;
 
 public class Workstation extends Tower implements CanBeRepaired {
 	public static final int SPRITE_Y_LEVEL = 7 * 32;
-	public static Rectangle SPRITE_RECTANGLE = new Rectangle(0, SPRITE_Y_LEVEL,
-			64, 64);
+	public static Rectangle SPRITE_RECTANGLE = new Rectangle(0, SPRITE_Y_LEVEL, 64, 64);
 	public static int[] ACTION_ZONE = { 34, 34 };
 	public static int WIDTH = 32;
 	public static int HEIGHT = 32;
@@ -22,22 +21,22 @@ public class Workstation extends Tower implements CanBeRepaired {
 		return workstation;
 	}
 
+	private int repair = 50;
+
 	public Workstation(int x, int y) {
 		super(x, y);
 		workstation = this;
-		setImage(GlobalVariables.getSprites().getSubimage(SPRITE_RECTANGLE.x,
-				SPRITE_RECTANGLE.y, SPRITE_RECTANGLE.width * 2,
-				SPRITE_RECTANGLE.height));
-		setWidth(WIDTH);
-		setHeight(HEIGHT);
-		setActionZone(ACTION_ZONE);
+		this.setImage(GlobalVariables.getSprites().getSubimage(SPRITE_RECTANGLE.x, SPRITE_RECTANGLE.y, SPRITE_RECTANGLE.width * 2, SPRITE_RECTANGLE.height));
+		this.setWidth(WIDTH);
+		this.setHeight(HEIGHT);
+		this.setActionZone(ACTION_ZONE);
 
 		MouseHandler.addEventObserver(new MouseHandler() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (getBounds().contains(e.getPoint())) {
-					System.out.println(getBounds() + " " + e.getPoint());
-					TowerShop.getTowerShop().setXY(getCenterPoint().x,getCenterPoint().y);
+				if (Workstation.this.getBounds().contains(e.getPoint())) {
+					System.out.println(Workstation.this.getBounds() + " " + e.getPoint());
+					TowerShop.getTowerShop().setXY(Workstation.this.getCenterPoint().x, Workstation.this.getCenterPoint().y);
 					TowerShop.getTowerShop().show(TowerShop.REPAIR, Workstation.this);
 				}
 			}
@@ -46,15 +45,12 @@ public class Workstation extends Tower implements CanBeRepaired {
 
 	@Override
 	public void draw(Graphics g) {
-		g.drawImage(
-				GlobalVariables.getSprites().getSubimage(SPRITE_RECTANGLE.x,
-						SPRITE_RECTANGLE.y, SPRITE_RECTANGLE.width,
-						SPRITE_RECTANGLE.height), getX(), getY(), null);
+		g.drawImage(GlobalVariables.getSprites().getSubimage(SPRITE_RECTANGLE.x, SPRITE_RECTANGLE.y, SPRITE_RECTANGLE.width, SPRITE_RECTANGLE.height), this.getX(), this.getY(), null);
 	}
 
 	/*
 	 * private int healthPoints;
-	 *
+	 * 
 	 * public int getHealthPoints() { return healthPoints; }
 	 */
 
@@ -63,6 +59,10 @@ public class Workstation extends Tower implements CanBeRepaired {
 	 */
 	@Override
 	public void repair() {
+		if (GlobalVariables.life <= 950) {
+			GlobalVariables.life += this.repair;
+		}
+
 	}
 
 	/*
